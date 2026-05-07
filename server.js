cf134feed2c366c60305f4b106ebab7fd428df2d
ndatimanayves2 <ndatimanayves4@gmail.com@example.com>
2026-05-07 17:29:33 +0200

    last commit

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ require('./src/models/OrderItem');
 require('./src/models/Notification');
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: '*', credentials: true }));
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
